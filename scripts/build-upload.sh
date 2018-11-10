c121b7852ba0423a6943d03353f93dc4f549360c
MrFrangipane <mister@frangipane.me>
2018-11-10 13:22:14 +0100

    fix script

--- scripts/build-upload.sh
+++ scripts/build-upload.sh
@@ -1,5 +1,4 @@
-cd ~/frangitron/frangitron-midibox
+cd ~/frangitron/frangitron-midibox/src
 make -j8
 cd ~/midibox/mios32/trunk/tools/mios_studio/Builds/Linux/build
-./MIOS_Studio_Debug --upload_hex=/home/frangi/frangitron/frangitron-midibox/project.hex
-
+./MIOS_Studio_Debug --upload_hex=~/frangitron/frangitron-midibox/src/project.hex
