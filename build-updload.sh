806350252f90381e3a56b5ffcf22786c5d7f2585
MrFrangipane <mister@frangipane.me>
2018-11-10 12:52:55 +0100

    Fix build script

--- build-updload.sh
+++
@@ -1,3 +0,0 @@
-cd /home/frangi/mios32/mios32
-make -j8
-bash /home/frangi/mios_studio.sh --upload_hex=/home/frangi/mios32/mios32/project.hex
---
+++ build-upload.sh
@@ -0,0 +1,3 @@
+cd /home/frangi/frangitron/frangitron-midibox
+make -j8
+bash /home/frangi/mios_studio.sh --upload_hex=/home/frangi/frangitron/frangitron-midibox/project.hex
